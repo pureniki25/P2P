4bb0158f5f2b8661f263a99e0be22d84560a7ed4
shengge25 <240435805@qq.com>
2024-11-14 20:02:18 +0800

    update

---
+++ schedule/tasks/task.go
@@ -0,0 +1,41 @@
+package tasks
+
+import (
+	"github.com/jasonlvhit/gocron"
+	"pledge-backend/db"
+	"pledge-backend/schedule/common"
+	"pledge-backend/schedule/services"
+	"time"
+)
+
+func Task() {
+
+	// get environment variables
+	common.GetEnv()
+
+	// flush redis db
+	err := db.RedisFlushDB()
+	if err != nil {
+		panic("clear redis error " + err.Error())
+	}
+
+	//init task
+	services.NewPool().UpdateAllPoolInfo()
+	services.NewTokenPrice().UpdateContractPrice()
+	services.NewTokenSymbol().UpdateContractSymbol()
+	services.NewTokenLogo().UpdateTokenLogo()
+	services.NewBalanceMonitor().Monitor()
+	//services.NewTokenPrice().SavePlgrPrice()
+
+	//run pool task
+	s := gocron.NewScheduler()
+	s.ChangeLoc(time.UTC)
+	_ = s.Every(2).Minutes().From(gocron.NextTick()).Do(services.NewPool().UpdateAllPoolInfo)
+	_ = s.Every(1).Minute().From(gocron.NextTick()).Do(services.NewTokenPrice().UpdateContractPrice)
+	_ = s.Every(2).Hours().From(gocron.NextTick()).Do(services.NewTokenSymbol().UpdateContractSymbol)
+	_ = s.Every(2).Hours().From(gocron.NextTick()).Do(services.NewTokenLogo().UpdateTokenLogo)
+	_ = s.Every(30).Minutes().From(gocron.NextTick()).Do(services.NewBalanceMonitor().Monitor)
+	//_ = s.Every(30).Minutes().From(gocron.NextTick()).Do(services.NewTokenPrice().SavePlgrPrice)
+	<-s.Start() // Start all the pending jobs
+
+}
